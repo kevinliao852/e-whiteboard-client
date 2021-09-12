c5efdcd9175f15767e1971a64c8c399c775eb3cb
kevinliao852 <q85292542000@gmail.com>
2021-09-12 15:53:01 +0800

    Add styling and contents on home page

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,18 +1,18 @@
-import { Menu } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react';
 import { Link, useParams } from 'react-router-dom';
-import { GoogleAuth, googleAuthProps } from './GoogleAuth'
+import { GoogleAuth, googleAuthProps } from './GoogleAuth';
 export const Header = (props: googleAuthProps) => {
-    return (
-        <Menu>
-            <Menu.Item active={useParams() === 'home'}>
-                <Link to="/home">Home</Link>
-            </Menu.Item>
-            <Menu.Item active={useParams() === 'dashboard'}>
-                <Link to="/dashboard" >Dashboard</Link>
-            </Menu.Item >
-
-            <GoogleAuth onAuthChange={props.onAuthChange} />
-        </Menu>
-    )
-
-}
+  return (
+    <Menu>
+      <Menu.Item active={useParams() === 'home'}>
+        <Link to="/home">Home</Link>
+      </Menu.Item>
+      <Menu.Item active={useParams() === 'dashboard'}>
+        <Link to="/dashboard">Dashboard</Link>
+      </Menu.Item>
+      <Menu.Item position="right">
+        <GoogleAuth onAuthChange={props.onAuthChange} />
+      </Menu.Item>
+    </Menu>
+  );
+};
--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
-import { Segment } from 'semantic-ui-react'
+import React from 'react';
+
+import { Segment, Image, Container } from 'semantic-ui-react';
+import '../css/style.css';
 export const Home = () => {
-    return (
-        <Segment>Welcome to the online whiteboard</Segment>
-    );
-}
+  return (
+    <>
+      <Container className="ui inverted vertical  center aligned segment">
+        <div className="ui text container">
+          <h1 className="ui inverted header">Online Whiteboard</h1>
+          <h2> You can draw whatever you want to</h2>
+          <div className="ui huge primary button">
+            Get Started <i className="right arrow icon"></i>
+          </div>
+        </div>
+      </Container>
+      <div className="ui vertical stripe segment">
+        <div className="ui middle aligned stackable grid container">
+          <div className="row">
+            <div className="eight wide column">
+              <h3 className="ui header">We Help Companies and School</h3>
+              <p>Express your idea with anywhere.</p>
+              <h3 className="ui header">We Make People Discuss Together</h3>
+              <p>Share your imagination via your creativity</p>
+            </div>
+            <div className="six wide right floated column">
+              <Image
+                style={{ background: 'blue' }}
+                className="card home-screen-image"
+                src="https://images.unsplash.com/photo-1537884557178-342a575d7d16?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
+              ></Image>
+            </div>
+          </div>
+          <div className="row">
+            <div className="center aligned column">
+              <a
+                className="ui huge button"
+                href="https://github.com/kevinliao852/online-whiteboard-client"
+              >
+                Contact us
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
