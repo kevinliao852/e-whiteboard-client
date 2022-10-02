be2c9140c8c1d0f8d39beda65a7be50c302e13d1
Kevin Liao <q85292542000@gmail.com>
2022-10-03 01:41:42 +0800

    feat: connect store and react

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import ReactDom from 'react-dom';
 import { App } from './component/App';
+import { Provider } from 'react-redux'
+import { store } from './app/store';
 
 ReactDom.render(
-    <App />,
+    <Provider store={store}>
+			<App />
+    </Provider>,
     document.querySelector("#root")
 );
 
