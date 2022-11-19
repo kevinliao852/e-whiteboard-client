d39e3afb8226274ec005d1d8ac246d86adf77bd9
Kevin Liao <q85292542000@gmail.com>
2022-11-20 04:35:25 +0800

    refactor: rearrange the side effects and types

--- src/contexts/GoogleAuthContext.tsx
+++ src/contexts/GoogleAuthContext.tsx
@@ -1,10 +1,17 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 const client_id = process.env.REACT_APP_GOOGLE_CLIENT_ID!;
 const host = process.env.REACT_APP_API_SERVER_HOST!;
 
-export const GoogleAuthContext = React.createContext({} as any);
+type GoogleAuthContextType = {
+  isSignedIn: Boolean;
+  signOut: () => void;
+  signIn: () => void;
+};
+
+export const GoogleAuthContext =
+  React.createContext<GoogleAuthContextType | null>(null);
 
 export const GoogleAuthContextStore = (props: any) => {
   const [isSignedIn, setIsSignedIn] = useState<Boolean>(false);
@@ -12,55 +19,38 @@ export const GoogleAuthContextStore = (props: any) => {
   useEffect(() => {
     gapi.load("auth2", () => {
       gapi.auth2.init({ client_id });
-      console.log(gapi.auth2);
-      console.log("gapi init");
+      gapi.auth2.getAuthInstance().isSignedIn.listen((signedIn) => {
+        console.log(signedIn);
+        setIsSignedIn(signedIn);
+        const idToken = gapi.auth2
+          .getAuthInstance()
+          .currentUser.get()
+          .getAuthResponse().id_token;
+
+        axios
+          .post(`${host}/login`, `idtoken=${idToken}`, {
+            headers: { "Access-Control-Allow-Credentials": true },
+            withCredentials: true,
+          })
+          .then(console.log)
+          .catch(console.log);
+      });
     });
   }, []);
 
-  useEffect(() => {
-    if (!gapi.auth2?.getAuthInstance()) {
-      return;
-    }
-    gapi.auth2.getAuthInstance().isSignedIn.listen((signedIn) => {
-      console.log(signedIn);
-      setIsSignedIn(signedIn);
-      const idToken = gapi.auth2
-        .getAuthInstance()
-        .currentUser.get()
-        .getAuthResponse().id_token;
-
-      axios
-        .post(`${host}/login`, `idtoken=${idToken}`, {
-          headers: { "Access-Control-Allow-Credentials": true },
-          withCredentials: true,
-        })
-        .then(console.log)
-        .catch(console.log);
-    });
-  }, [gapi.auth2?.getAuthInstance]);
-
-  useEffect(() => {
-    if (!gapi.auth2?.getAuthInstance()) {
-      return;
-    }
-    if (gapi.auth2.getAuthInstance().currentUser.get().isSignedIn()) {
-      setIsSignedIn(true);
-    }
-  });
-
-  const signIn = () => {
+  const signIn = useCallback(() => {
     console.log(gapi.auth2.getAuthInstance().signIn());
-  };
+  }, []);
 
-  const signOut = () => {
+  const signOut = useCallback(() => {
     gapi.auth2.getAuthInstance().signOut();
     setIsSignedIn(false);
-  };
+  }, []);
 
-  const valueProvider = { isSignedIn, signOut, signIn };
+  const value = { isSignedIn, signOut, signIn };
 
   return (
-    <GoogleAuthContext.Provider value={valueProvider}>
+    <GoogleAuthContext.Provider value={value}>
       {props.children}
     </GoogleAuthContext.Provider>
   );
