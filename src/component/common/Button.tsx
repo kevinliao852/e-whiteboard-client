8dc73610daa9d81daf8207d15384c8cdffb44db1
Kevin Liao <q85292542000@gmail.com>
2022-11-20 20:23:37 +0800

    feat: add common components

---
+++ src/component/common/Button.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: #dcdcdc;
+  padding: 0.5rem 1rem;
+  border-radius: 0.75rem;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
---
+++ src/component/common/Container.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1280px;
+  margin: auto;
+  padding: 2rem;
+  background-color: #d3d3d3;
+`;
