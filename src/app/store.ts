d8e95f4a42b0ef51b529c3ee2d90195da523def0
Kevin Liao <q85292542000@gmail.com>
2023-02-09 23:31:58 +0800

    feat(store): add user-slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import whiteboardReducer from "../features/whiteboard/whiteboard-slice";
 import authReducer from "../features/auth/auth-slice";
+import userReducer from "../features/user/user-slice";
 
 export const store = configureStore({
   reducer: {
     whiteboard: whiteboardReducer,
     auth: authReducer,
+    user: userReducer,
   },
   devTools: true,
 });
