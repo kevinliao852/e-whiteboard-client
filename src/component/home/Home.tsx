6fe0713a9f641b2e5cfc066331510212efd6db81
Kevin Liao <q85292542000@gmail.com>
2023-06-18 05:33:37 +0800

    feat: update home page

--- src/component/home/Home.tsx
+++ src/component/home/Home.tsx
@@ -1,13 +1,22 @@
-import React from "react";
-
-import { Segment, Image, Container } from "semantic-ui-react";
 import "../../css/style.css";
 export const Home = () => {
   return (
     <div className="ui raised very padded text container segment">
-      <div className="ui text container">
+      <div className="ui text ">
         <h1 className="ui header">e-whiteboard</h1>
-        <p>Express the thought on the whiteboard!!!</p>
+        <div className="ui segment">
+          <p>
+            This is a virtual whiteboard application where you can collaborate
+            with others in real-time. Create and join whiteboards to brainstorm
+            ideas, sketch diagrams, or work on projects together.
+          </p>
+        </div>
+        <div className="ui segment">
+          <p>
+            Get started by signing up or logging in to your account. Once logged
+            in, you can create your own whiteboards or join existing ones.
+          </p>
+        </div>
       </div>
     </div>
   );
